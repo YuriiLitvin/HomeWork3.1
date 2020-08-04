b207856382959fc2b5e6c5082d3423b0abac1a4e
Yurii Litvin <yurijlitviniii@gmail.com>
2020-08-04 14:13:51 +0300

    Add project files.

---
+++ Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace HomeWork3
+{
+    public static class Helper
+    {
+        public static string GetElementsString(this List<int> list)
+        {
+            string str = "";
+            foreach (var item in list)
+            {
+                str += $"{item} ,";  
+            }
+            return str;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testList = new List<int> { 1, 2, 3, 4 };
+            Console.WriteLine(testList.GetElementsString());
+            Console.ReadLine();
+        }
+        
+    }
+
+
+
+}
